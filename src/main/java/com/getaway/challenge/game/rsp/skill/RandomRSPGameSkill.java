daaa75e13b5a1e157dea0911c1db3f05420a034c
mfenenko <mfenenko-scf@transparent.com>
2026-07-25 22:16:11 +0200

    - properly use ThreadLocalRandom and create thread local instance over using one shared

--- src/main/java/com/getaway/challenge/game/rsp/skill/RandomRSPGameSkill.java
+++ src/main/java/com/getaway/challenge/game/rsp/skill/RandomRSPGameSkill.java
@@ -1,7 +1,6 @@
 package com.getaway.challenge.game.rsp.skill;
 
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 import com.getaway.challenge.game.rsp.RSPGameOptions;
@@ -13,20 +12,17 @@ public class RandomRSPGameSkill implements RSPGameSkill {
 
     private List<RSPGameOptions> options;
     
-    private Random random;
-    
     /**
      * 
      * @param options
      */
     public RandomRSPGameSkill(List<RSPGameOptions> options) {
         this.options = options;
-        this.random = ThreadLocalRandom.current();
     }
     
     
     public RSPGameOptions next() {
-        return options.get( random.nextInt(options.size()) );
+        return options.get(ThreadLocalRandom.current().nextInt(options.size()));
     }
     
 }
